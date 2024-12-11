75876357612aceaed701000c3928d0e55b8a928f
Viktor Pratama <147905833+Viktorianpratama@users.noreply.github.com>
2024-12-12 04:49:33 +0900

    Update Login.jsx

--- rantaucash/src/pages/Login.jsx
+++ rantaucash/src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/users/login', {
+      const response = await axios.post('https://backendw-blush.vercel.app/?vercelToolbarCode=KLDbibxCv_Gs8zu/api/users/login', {
         email,
         password,
       });
