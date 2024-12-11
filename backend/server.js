6231fa00509a702b5e3c13e703d413e540bb8bd8
Viktor Pratama <147905833+Viktorianpratama@users.noreply.github.com>
2024-12-12 02:37:04 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,6 @@
 import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { createClient } from '@supabase/supabase-js';
-import userRoutes from './routes/userRoutes.js'
-import paymentRoutes from './routes/paymentRoutes.js';
-import roomsRoutes from './routes/roomsRoutes.js';
-import notificationRoutes from './routes/notificationRoutes.js';
 
-dotenv.config();
+
 const app = express();
 
 
@@ -16,5 +9,5 @@ res.send ("server berjalan");
 }
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
