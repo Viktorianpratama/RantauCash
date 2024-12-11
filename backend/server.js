c6c2aa16380a0f0c8e6b4001a68e9bdd61dea5f7
Viktor Pratama <147905833+Viktorianpratama@users.noreply.github.com>
2024-12-12 02:29:43 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,29 +10,8 @@ import notificationRoutes from './routes/notificationRoutes.js';
 dotenv.config();
 const app = express();
 
-// Validasi jika Supabase URL atau Key tidak tersedia
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
-  throw new Error('Missing SUPABASE_URL or SUPABASE_KEY in environment variables');
-}
 
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-console.log('Supabase initialized:', supabase);
-
-app.use(cors({
-  origin: 'https://rantau-cash-client.vercel.app/',  // Atur sesuai dengan domain frontend Anda
-}));
-
-app.use(express.json());
-
-// Routes
-app.use('/api/users', userRoutes(supabase));
-app.use('/api/payments', paymentRoutes(supabase));
-app.use('/api/rooms', roomsRoutes(supabase));
-app.use('/api', notificationRoutes(supabase));
-
-
-app.use("/",(req,res) => {
+app.use("/home",(req,res) => {
 res.send ("server berjalan");
 }
 
