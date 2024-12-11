1dbe2ac3bb1051f29bc36ca3c3cd50dedf6da74c
Viktor Pratama <147905833+Viktorianpratama@users.noreply.github.com>
2024-12-12 02:56:43 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,9 @@
-import express from 'express';
-
-
+const express = require('express');
 const app = express();
 
-
-app.use("/",(req,res) => {
-res.send ("server berjalan");
-}
+app.get("/", (req, res) => {
+    res.send("server berjalan");
+});
 
 // Start Server
 const PORT = 5000;
